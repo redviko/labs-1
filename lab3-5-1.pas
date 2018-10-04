8d6d6ff76d679b9fbf3932b4bb44e5a0b21d5996
alianse777 <alianse777@users.noreply.github.com>
2018-10-04 14:18:06 +0500

    Update lab3-5-1.pas

--- lab3-5-1.pas
+++ lab3-5-1.pas
@@ -4,17 +4,28 @@ uses crt;
 
 var a, b, c, x: real;
 
+function sdiv(a,b: real): real;
+begin
+	if b = 0 then
+	begin
+	Writeln('division by zero');
+	exit;
+	end
+	else
+	sdiv := a/b;
+end;
+
 begin
   Readln(a, b, c, x);
   if (c < 0) and (b <> 0) then
-  begin
-  writeln(-a*x*x + 5);
-  end
+	begin
+	writeln(-a*x*x + 5);
+	end
   else
-    if (c > 0) and (c = 0) then
-    begin
-    writeln((a + 4*x) / (c*x));
-    end
+    if (c > 0) and (a = 0) then
+    	begin
+    	writeln((a + 4*x) / (c*x));
+    	end
     else
-    writeln(1 + x/(c + 5));
-    end.
+    	writeln(1 + x/(c + 5));
+end.
